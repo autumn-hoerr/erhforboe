78fd4d0f38895a7cf4784efe73ca510e7098ef8d
Dan Hoerr <dhoerr@gmail.com>
2018-01-22 07:22:12 -0500

    fixes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,13 +13,16 @@ const Header = () => (
       <Link to="/" exact className="c-main-nav__link--home">
         <img src={logo} alt="ERH for BOE" title="Home" />
       </Link>
-      <nav className="c-navigation">
-        <Link to="/about" exact activeClassName="is-active" className="c-main-nav__link">About</Link>
-        <Link to="/issues" exact activeClassName="is-active" className="c-main-nav__link">Issues</Link>
-        <a href="https://donorbox.org/elena-rosemond-hoerr-for-school-board" className="c-main-nav__link--donate">Donate</a>
-      </nav>
-      <h2 className="h-mobile-only c-social-links__intro">Stay connected:</h2>
-      <SocialLinks />
+      <div className="navigation">
+        <nav className="c-navigation">
+          <Link to="/" exact activeClassName="is-active" className="h-mobile-only c-main-nav__link">Home</Link>
+          <Link to="/about" exact activeClassName="is-active" className="c-main-nav__link">About</Link>
+          <Link to="/issues" exact activeClassName="is-active" className="c-main-nav__link">Issues</Link>
+          <a href="https://donorbox.org/elena-rosemond-hoerr-for-school-board" className="c-main-nav__link--donate">Donate</a>
+        </nav>
+        <h2 className="h-mobile-only c-social-links__intro">Stay connected:</h2>
+        <SocialLinks />
+      </div>
     </div>
   </header>
 )
